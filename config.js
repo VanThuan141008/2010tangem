7adf8d88fd2bf2dc0302228f0583688b12508adc
Van Thuan <vanthuan144008@gmail.com>
2023-10-16 19:53:45 +0700

    git project

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = 'Em';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Chúc Mừng 20-10';
+const titleCard = 'Tặng em';
+const contentCard = 'Chúc vợ của anh 20/10 tràn ngập niềm vui và những nụ cười , mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau này';
+
+
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
